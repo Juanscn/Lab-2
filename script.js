a27022a5e92525163cba23fbfe378c7c585ebcb9
Juanscn <juan.camelo@edu.sait.ca>
2024-11-06 16:13:00 -0700

    Added shadows

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ bearButton.addEventListener('click', function(){
 elkButton.addEventListener('click', function(){
     document.getElementById('image').src = 'images/elk.webp';
 })
+
+mooseButton.addEventListener('click', function(){
+    document.getElementById('image').src = 'images/moose.webp';
+})
+
+sheepButton.addEventListener('click', function(){
+    document.getElementById('image').src = 'images/sheep.webp';
+})
+
+wolfButton.addEventListener('click', function(){
+    document.getElementById('image').src = 'images/wolf.webp';
+})
