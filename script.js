bf2d17d456a096498aba0174add62f2b90f230cf
Juanscn <juan.camelo@edu.sait.ca>
2024-11-06 16:01:51 -0700

    Started JS

---
+++ script.js
@@ -0,0 +1,7 @@
+bearButton.addEventListener('click', function(){
+    document.getElementById('image').src = 'images/bear.webp';
+})
+
+elkButton.addEventListener('click', function(){
+    document.getElementById('image').src = 'images/elk.webp';
+})
